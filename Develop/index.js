0897af494cc4f9e58a7747c706db836544e84487
Caleb Chaney <calebc347@gmail.com>
2020-10-02 09:03:50 -0500

    Updated Jascript

--- Develop/index.js
+++ Develop/index.js
@@ -21,18 +21,18 @@ function promptUser() {
         },
         {
             type: "input",
-            name: "Table of Contents",
-            question: "Table of contents for application"
+            name: "Table",
+            question: "Table of contents for README."
         },
         {
             type: "input",
             name: "Installation",
-            question: "How do you install the application?"
+            question: "What are the steps required to install your project?"
         },
         {
             type: "input",
             name: "Usage",
-            question: "How do you use the application?"
+            question: "Provide instruction and examples for use."
         },
         {
             type: "input",
@@ -47,7 +47,7 @@ function promptUser() {
         {
             type: "input",
             name: "Tests",
-            question: "?"
+            question: "Write tests for your application and provide examples on how to run them."
         },
         {
             type: "input",
@@ -59,28 +59,29 @@ function promptUser() {
 
 function generateMd(answers) {
     return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-      <p class="lead">I am from ${answers.location}.</p>
-      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${answers.github}</li>
-        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-      </ul>
-    </div>
-  </div>
-  </body>
-  </html>`;
+    # ${answers.Title}
+    
+    ## Description
+    ${answers.Description}
+    
+    ## Table of Contents
+    ${answers.Table}
+    
+    ## Installation
+    ${answers.Installation}
+
+    ## Usage
+    ${answers.Usage}
+
+    ## License
+    ${answers.License}
+
+    ## Contributing 
+    ${answers.Tests}
+
+    ## Questions
+    ${answers.Questions}
+    `;
 }
 
 promptUser()
